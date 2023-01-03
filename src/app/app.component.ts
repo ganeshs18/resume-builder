f5e4763ce00bf447d6f0f0c7175510d052b37c78
Ganesh S <ganesh.sundaramoorthy@wipro.com>
2023-01-03 10:38:28 +0530

    added reactive form partially

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MatDatepickerToggle } from '@angular/material/datepicker';
+import { Component } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
 import { EditorModalComponent } from './editor-modal/editor-modal.component';
@@ -12,25 +11,22 @@ import { EditorModalComponent } from './editor-modal/editor-modal.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'resume-builder';
-  htmlContent: any;
-  mobileControl = new FormControl('')
+  resumeForm!: FormGroup;
   editorConfig: AngularEditorConfig = {
     showToolbar: false,
     editable: false
   }
-  toggle!: MatDatepickerToggle<any>;
-  constructor(public dialog: MatDialog) {
-    // this.toggle._open()
+  constructor(public dialog: MatDialog, private formBuilder: FormBuilder) {
+    this.buildForm();
   }
 
-  openEditor(title: string, content?: any) {
+  openEditor(title: string, formControl?: AbstractControl | null) {
     const dialogRef = this.dialog.open(EditorModalComponent, {
       disableClose: true,
       height: '500px',
       width: '800px',
       data: {
-        content,
+        control: formControl as FormControl,
         title
       }
     });
@@ -41,6 +37,45 @@ export class AppComponent {
   }
 
 
+  buildForm() {
+    this.resumeForm = this.formBuilder.group({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      profession: new FormControl(''),
+      mobile: new FormControl('', [Validators.required, Validators.pattern(/[6-9][0-9]{9}/)]),
+      social: this.formBuilder.array([this.getSocialForm()]),
+      profSummary: new FormControl('')
+    })
+  }
+
+
+  getSocialForm(): FormGroup {
+    return this.formBuilder.group({
+      siteName: new FormControl('', [Validators.required]),
+      url: new FormControl('', [Validators.required])
+    })
+  }
+
+  get socialFormArrayControls(): FormGroup[] {
+    return this.socialFormArray.controls as FormGroup[];
+  }
+
+  private get socialFormArray(): FormArray {
+    return this.resumeForm.get('social') as FormArray;
+  }
+
+  addSocialLink(): void {
+    this.socialFormArray.push(this.getSocialForm());
+  }
+
+  deleteSocialLink(index: number): void {
+    this.socialFormArray.removeAt(index);
+  }
+
+  getFormControl(control: AbstractControl | null): FormControl {
+    return control as FormControl;
+  }
 }
 
 
--- src/app/editor-modal/editor-modal.component.ts
+++ src/app/editor-modal/editor-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
 import { editorConfig } from '../config/editor.config';
@@ -12,10 +13,16 @@ export class EditorModalComponent {
 
   htmlContent: any;
   editorConfig: AngularEditorConfig = editorConfig;
-
+  formControl: FormControl = new FormControl('');
   constructor(private dialogRef: MatDialogRef<EditorModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
+    this.formControl = this.data?.control;
+    this.htmlContent = this.formControl?.value;
+  }
 
+  onSave() {
+    this.formControl?.setValue(this.htmlContent);
+    this.dialogRef.close();
   }
 
 }
